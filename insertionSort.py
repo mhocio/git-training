29304646b1811a678f6bc6c3d69e5ddca5f08085
mhocio <mikolajhoscilo@gmail.com>
2020-03-28 12:52:47 +0100

    Add source code for the insertion sort

---
+++ insertionSort.py
@@ -0,0 +1,23 @@
+import  random
+
+random_items = [random.randint(0,100) for c in range(10)]
+
+def insertion_sort(items):
+    count = 0
+    for i in range(1, len(items)):
+        j = i
+        # count += 1
+        while j > 0 and items[j] < items[j-1]:
+            items[j], items[j-1] = items[j-1], items[j]
+            j -= 1
+            count += 1
+        
+        print(i, count, items)
+    return count
+
+
+count = insertion_sort(random_items)
+print(random_items)
+print(count)
+
+
