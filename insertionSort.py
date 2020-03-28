82b7379b9c3d784da7deb53fd5c7552dec740cd8
mhocio <mikolajhoscilo@gmail.com>
2020-03-28 13:12:38 +0100

    Update random_items len, change...

--- insertionSort.py
+++ insertionSort.py
@@ -1,24 +1,24 @@
 import  random
 
-random_items = [random.randint(0,100) for c in range(10)]
+random_items = [random.randint(0,100) for c in range(12)]
 
 def insertion_sort(items):
-    count = 0
+    count = 10
     for i in range(1, len(items)):
         j = i
         # count += 1
         while j > 0 and items[j] < items[j-1]:
             items[j], items[j-1] = items[j-1], items[j]
             j -= 1
-            count += 1
+            count += 2
         
         print(i, count, items)
     return count
 
 
-count = insertion_sort(random_items)
+countSwaps = insertion_sort(random_items)
 print(random_items)
-print(count)
+print(countSwaps)
 
 print("test")
 
