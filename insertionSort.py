7382d70e305f27bcdeef5b07c9eac64d9e1f4c70
mhocio <mikolajhoscilo@gmail.com>
2020-03-28 13:17:10 +0100

    Change sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,12 +1,16 @@
 import  random
 
+<<<<<<< HEAD
 random_items = [random.randint(0,100) for c in range(12)]
+=======
+random_items = [random.randint(0,100) for c in range(6)]
+>>>>>>> Change sort
 
 def insertion_sort(items):
     count = 10
     for i in range(1, len(items)):
         j = i
-        # count += 1
+        count += 1
         while j > 0 and items[j] < items[j-1]:
             items[j], items[j-1] = items[j-1], items[j]
             j -= 1
@@ -16,9 +20,15 @@ def insertion_sort(items):
     return count
 
 
+<<<<<<< HEAD
 countSwaps = insertion_sort(random_items)
 print(random_items)
 print(countSwaps)
+=======
+countA = insertion_sort(random_items)
+print(random_items)
+print(countA)
+>>>>>>> Change sort
 
 print("test")
 
